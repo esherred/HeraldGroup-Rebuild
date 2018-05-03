2bb9e8c59866ca6fa644ab8e6920ee7b13bf234d
Eric Sherred <>
2018-05-03 13:10:27 -0500

    favicon and logo optimization

--- wp-content/themes/thg-v2/templates/_partials/_head.php
+++ wp-content/themes/thg-v2/templates/_partials/_head.php
@@ -4,7 +4,9 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Herald Group Rebuild</title>
+    <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+    <link href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico" rel="shortcut icon">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico" rel="apple-touch-icon-precomposed">
 
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
   </head>
